3781338f14321f58f87e959ecf9f8c039d9aa836
bemax3 <bemax.ng@gmail.com>
2022-02-09 18:00:18 +0000

    FizzBuzz with new requirements

--- src/main/java/com/example/gamma/test/App.java
+++ src/main/java/com/example/gamma/test/App.java
@@ -13,7 +13,8 @@ public final class App {
      * @param args The arguments of the program.
     */
     public static void main(String[] args) {
-        fizzBuzz(100);
+        // fizzBuzz(100);
+        fizzBuzzV2(100);
     }
     
     /**
@@ -26,6 +27,18 @@ public final class App {
         IntStream.range(1, limit+1)
         .mapToObj(number -> (number % 3 == 0) ? (number % 5 == 0 ? "FizzBuzz" : "Fizz") : (number % 5 == 0 ? "Buzz" : number))
         .forEach(System.out::println);
-        
+    }   
+
+    /**
+     * Prints to the console the numbers from 1 to the limit given.
+     * But for multiples of 3 or numbers that have 3 in it prints Fizz instead 
+     * And for the multiples of 5 or numbers that have 5 in it prints Buzz.
+     * For the multiples of both 3 and 5 it prints FizzBuzz.
+     * @param limit The Limit of numbers printed (inclusive) starting from 1.
+    */
+    protected static void fizzBuzzV2(int limit) {
+        IntStream.range(1, limit+1)
+        .mapToObj(number -> (number % 3 == 0 || String.valueOf(number).contains("3")) ? ((number % 5 == 0 || String.valueOf(number).contains("5")) ? "FizzBuzz" : "Fizz") : ((number % 5 == 0 || String.valueOf(number).contains("5")) ? "Buzz" : number))
+        .forEach(System.out::println);
     }   
 }
