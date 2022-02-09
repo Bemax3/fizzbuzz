3a96c8f4583c1780dfc16aa0c27e252562605b32
bemax3 <bemax.ng@gmail.com>
2022-02-09 17:41:01 +0000

    Test With numbers from 1 to 15

--- src/main/java/com/example/gamma/test/App.java
+++ src/main/java/com/example/gamma/test/App.java
@@ -6,7 +6,7 @@ import java.util.stream.IntStream;
  * This is a simple class that implements the FizzBuzz game
 */
 public final class App {
-    private App() {}
+    protected App() {}
 
     /**
      * Main Function just execute the fizzBuzz method with 100 first numbers
@@ -22,9 +22,10 @@ public final class App {
      * For the multiples of both 3 and 5 it prints FizzBuzz.
      * @param limit The Limit of numbers printed (inclusive) starting from 1.
     */
-    private static void fizzBuzz(int limit) {
+    protected static void fizzBuzz(int limit) {
         IntStream.range(1, limit+1)
         .mapToObj(number -> (number % 3 == 0) ? (number % 5 == 0 ? "FizzBuzz" : "Fizz") : (number % 5 == 0 ? "Buzz" : number))
         .forEach(System.out::println);
+        
     }   
 }
--- src/test/java/com/example/gamma/test/AppTest.java
+++ src/test/java/com/example/gamma/test/AppTest.java
@@ -1,18 +1,38 @@
 package com.example.gamma.test;
 
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 /**
- * Unit test for simple App.
+ * Unit test for simple fizzbuzz.
  */
 class AppTest {
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    @BeforeEach
+    public void setUpStream() {
+        System.setOut(new PrintStream(out));
+    }
+
+    @AfterEach
+    public void restoreStream() {
+        System.setOut(originalOut);
+    }
     /**
      * Rigorous Test.
      */
     @Test
-    void testApp() {
-        assertEquals(1, 1);
+    public void testFizz() {
+        App.fizzBuzz(15);
+        String expected = "1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\nBuzz\n11\nFizz\n13\n14\nFizzBuzz";
+        assertEquals(expected,out.toString().trim());
     }
 }
